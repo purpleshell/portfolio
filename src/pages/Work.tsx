0144ef4ad1b124f1f8cebe1289e48dbab84a86ae
purpleshell <purpleshell@github.com>
2019-04-29 23:16:34 -0400

    added portfolio links

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -9,49 +9,51 @@ export default ({ match }: any) => {
       <div className="portfolio">
         <h1 className="portfolio-title">Previous Work</h1>
         <ol className="portfolio-items">
+          <li className="portfolio-item overload">
+            <a href="https://overload-client.herokuapp.com/exercises">
+              <div className="portfolio-item-text-bg">
+                <div className="portfolio-item-text">
+                  <h2>Overload</h2>
+                  <p>
+                    A full-stack React/TypeScript PWA I single handedly took
+                    from inception through design and into implementation. It is
+                    intednded to function as a personal database for users to
+                    log their resistance training progress.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li className="portfolio-item grid-example">
+            <a href="#">
+              <div className="portfolio-item-text-bg">
+                <div className="portfolio-item-text">
+                  <h2>CSS Grid Example</h2>
+                  <p>
+                    A quick mockup of a blog index which I made while learning
+                    CSS Grid.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li className="portfolio-item vaster">
+            <a href="https://serene-brown-68227f.netlify.com/">
+              <div className="portfolio-item-text-bg">
+                <div className="portfolio-item-text">
+                  <h2>Vaster</h2>
+                  <p>
+                    Completely from scratch React/TypeScript PWA. Responsive
+                    mobile/desktop design using the antd library. Current
+                    functionalities include: account registration, log-in,
+                    forgot password and a mock storefront using RandomUser
+                    public API.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </li>
           <li className="portfolio-item first-work">
-            <a href="#">
-              <div className="portfolio-item-text-bg">
-                <div className="portfolio-item-text">
-                  <h2>Humble Beginnings (This Web-Site)</h2>
-                  <p>
-                    My first vanilla HTML/CSS project, which I made for a
-                    friend. I improved and ported it to this basic react app.
-                    Original files and proof of hand-off available upon request.
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li className="portfolio-item second-work">
-            <a href="#">
-              <div className="portfolio-item-text-bg">
-                <div className="portfolio-item-text">
-                  <h2>Humble Beginnings (This Web-Site)</h2>
-                  <p>
-                    My first vanilla HTML/CSS project, which I made for a
-                    friend. I improved and ported it to this basic react app.
-                    Original files and proof of hand-off available upon request.
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li className="portfolio-item third-work">
-            <a href="#">
-              <div className="portfolio-item-text-bg">
-                <div className="portfolio-item-text">
-                  <h2>Humble Beginnings (This Web-Site)</h2>
-                  <p>
-                    My first vanilla HTML/CSS project, which I made for a
-                    friend. I improved and ported it to this basic react app.
-                    Original files and proof of hand-off available upon request.
-                  </p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li className="portfolio-item fourth-work">
             <a href="#">
               <div className="portfolio-item-text-bg">
                 <div className="portfolio-item-text">
@@ -69,7 +71,9 @@ export default ({ match }: any) => {
       </div>
 
       <div className="cta-btn">
-        <a className="cta-link">Get In Touch</a>
+        <a className="cta-link" href="mailto:admin@purpleshell.xyz">
+          Hire Me
+        </a>
       </div>
     </>
   );
